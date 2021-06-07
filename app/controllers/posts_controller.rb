afceb53a00ede02c42bf9463f73b0635b460e921
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-06-07 12:03:23 +1000

    Able to ediut and update posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,23 +7,29 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def edit
+		@post = Post.find(params[:id])
+	end
+
 	def new
 		@post = Post.new
 	end
 
 	def create
-	  @post = Post.new(params["post"])
+	  @post = Post.new(post_params(:title, :description))
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
+		@post = Post.find(params[:id])
+		@post.update(post_params(:title, :description))
+		redirect_to post_path(@post)
 	end
 
-	def edit
-	  @post = Post.find(params[:id])
+	private
+
+	def post_params(*args)
+	params.require(:post).permit(*args)
 	end
 end
